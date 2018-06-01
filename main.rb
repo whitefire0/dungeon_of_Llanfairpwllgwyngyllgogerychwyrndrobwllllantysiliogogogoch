7c98316f0c272fbd365356f9b63cf85a51ff7adb
whitefire0 <rickhallett@live.co.uk>
2018-06-01 23:45:10 +0100

    abstracted base attribute assignment into seperate module for re-use amongst character type classes

--- main.rb
+++ main.rb
@@ -2,10 +2,27 @@ require 'pry'
 require 'pry-byebug'
 require 'awesome_print'
 
+module BaseAttributes
+
+  def create_base(attributes)
+    @name = attributes[:name]
+    @age = attributes[:age]
+    @health = attributes[:health]
+    @strength = attributes[:strength]
+    @constitution = attributes[:constitution]
+    @dexterity = attributes[:dexterity]
+    @intelligence = attributes[:intelligence]
+  end
+
+end
+
 class Game
 end
 
 class Character
+
+  include BaseAttributes
+
   def self.create(name)
     age = rand * 60 + 15
     attributes = {
@@ -23,23 +40,20 @@ class Character
   def self.base_range
     rand * 50 + 15
   end
+
 end
 
 class Enemy
 end
 
 class Viking < Character
-  attr_reader :name, :age, :health, :strength
+
+  attr_reader :name
+
   def initialize(attributes)
-    @name = attributes[:name]
-    @age = attributes[:age]
-    @health = attributes[:health]
-    @strength = attributes[:strength]
-    @constitution = attributes[:constitution]
-    @dexterity = attributes[:dexterity]
-    @intelligence = attributes[:intelligence]
+    create_base(attributes)
     roll_attribute_modifiers
-    # roll_unique_attributes
+    roll_unique_attributes
   end
 
   def roll_attribute_modifiers
@@ -50,35 +64,49 @@ class Viking < Character
   def roll_unique_attributes
 
   end
+
 end
 
 class Wizard < Character
-  attr_reader :name, :age, :health, :strength
+
+  attr_reader :name
+
   def initialize(attributes)
-    @name = attributes[:name]
-    @age = attributes[:age]
-    @health = attributes[:health]
-    @strength = attributes[:strength]
-    @constitution = attributes[:constitution]
-    @dexterity = attributes[:dexterity]
-    @intelligence = attributes[:intelligence]
-    # roll_unique_attributes
+    create_base(attributes)
+    roll_attribute_modifiers
+    roll_unique_attributes
+  end
+
+  def roll_attribute_modifiers
+    @strength *= 1.5
+    @health += 200
+  end
+
+  def roll_unique_attributes
+
   end
 
 end
 
 class Rogue < Character
-  attr_reader :name, :age, :health, :strength
+
+  attr_reader :name
+
   def initialize(attributes)
-    @name = attributes[:name]
-    @age = attributes[:age]
-    @health = attributes[:health]
-    @strength = attributes[:strength]
-    @constitution = attributes[:constitution]
-    @dexterity = attributes[:dexterity]
-    @intelligence = attributes[:intelligence]
-    # roll_unique_attributes
+    create_base(attributes)
+    roll_attribute_modifiers
+    roll_unique_attributes
   end
+
+  def roll_attribute_modifiers
+    @strength *= 1.5
+    @health += 200
+  end
+
+  def roll_unique_attributes
+
+  end
+  
 end
 
 class Goblin < Enemy
