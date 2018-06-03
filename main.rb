6a3859f67af95680cf8cfea7d15c251a07347413
whitefire0 <rickhallett@live.co.uk>
2018-06-03 20:39:20 +0100

    bug: create_player and create_character_instance call each other creating problematic stack overloading

--- main.rb
+++ main.rb
@@ -6,39 +6,56 @@ require_relative 'characters'
 require_relative 'enemies'
 
 class Game
-  attr_accessor :on, :player_created, :player_name, :player_char
+  attr_accessor :on, :player_created, :player_name, :player_char, :menu_instance
 
   def initialize
+    @menu_instance = nil
     @on = true
     @player_created = false
     @player_name = nil
     @player_char = nil
   end
- 
-  # class << self
-  #   attr_accessor :on
+
+  # WHYYY
+  # main.rb:21:in `instance_variable_set': `menu_interface' is not allowed as an instance variable name (NameError)
+  # def inject_dependency(var_name, object_name)
+  #   var_name = var_name.to_sym
+  #   instance_variable_set(var_name, object_name)
   # end
 
   def create_character_instance(chosen_class)
     case chosen_class
-    when /[vV]/
-      @player_char = Object.const_get('Viking')
-    when /[bB]/
-      @player_char = Object.const_get('Barbarian')
-    when /[wW]/
-      @player_char = Object.const_get('Wizard')
-    when /[rR]/
-      @player_char = Object.const_get('Rogue')
-    when /[cC]/
-      @player_char = Object.const_get('Cleric')
-    when /[gG]/
-      @player_char = Object.const_get('Gimp')
+    when /^v|V/
+      set_player_class('Viking')
+    when /^b|B/
+      set_player_class('Barbarian')
+    when /^w|W/
+      set_player_class('Wizard')
+    when /^r|R/
+      set_player_class('Rogue')
+    when /^c|C/
+      set_player_class('Cleric')
+    when /^g|G/
+      set_player_class('Gimp')
     else
+      puts "Invalid choice, please choose again."
+      @menu_instance.create_player
     end
 
-    @player_char = @player_char.create(@player_name)
-    @player_char.name << " the #{@player_char.class}"
-    puts @player_char.name
+    if @player_char
+      # HACK: prevent recreation of character if already established from second+ call of create_player i.e. if user entered incorrect character type first time (limit print statement to once due to create_player and create_character_instance calling each other creating repeated stack frames)
+      # FIX: two loops calling each other - try inner method conditional looping
+      unless @player_char.instance_variables
+        binding.pry
+        @player_char = @player_char.create(@player_name)
+        @player_char.name << " the #{@player_char.class}"
+      end
+
+    end
+  end
+
+  def set_player_class(classname)
+    @player_char = Object.const_get(classname)
   end
 end
 
@@ -67,17 +84,27 @@ class UserInterface
   end
 
   def create_player
-    puts "Please enter your name, player: \n"
-    @game_instance.player_name = gets.chomp
-    @player_created = true
-    puts "Please choose your character class (v, b,w, r, c, g):\n"
+    unless @player_created
+      puts "Please enter your name, player: \n"
+      @game_instance.player_name = gets.chomp
+      @player_created = true
+    end
+    puts "\nPlease choose your character class (v, b,w, r, c, g):\n"
     chosen_class = gets.chomp
     @game_instance.create_character_instance(chosen_class)
+
+    # HACK: limit print statement to once due to create_player and create_character_instance calling each other creating repeated stack frames
+    print_num = 0
+    if print_num == 0
+      puts "Welcome to the dungeon, #{@game_instance.player_name}! Untold glory awaits you."
+      print_num =+ 1
+    end
   end
 end
 
 game = Game.new
 interface = UserInterface.new(game)
+game.menu_instance = interface
 interface.run_menu
 
 
